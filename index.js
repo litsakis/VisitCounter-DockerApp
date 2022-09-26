64c12fae69dffcbb5d2196976dad51b337f4d6cd
Alexandros Litsakis <alexandroslitsakis@gmail.com>
2022-09-26 20:18:59 +0300

    fixing some typos

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const redis = require('redis')
 
 const app = express();
 const client = redis.createClient();
+client.set('visits',0);
 
 app.get('/',(req,res) => {
     client.get('visits', (err, visits) =>{
-        res.send('Number of visits is + visits')
-    } )
+        res.send('Number of visits is' + visits);
+        client.set('visits', parseInt(visits) +1);
+    } );
+});
+
+app.listen(8082, () => {
+    console.log ('Listening on port 8082');
 });
